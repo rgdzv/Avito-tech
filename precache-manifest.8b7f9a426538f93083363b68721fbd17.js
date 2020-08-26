281bb75fec4ae5297ffb99e338030d703a39fb30
rgdzv <r_gadzhiomarov@mail.ru>
2020-08-26 11:02:18 +0300

    Updates

---
+++ precache-manifest.8b7f9a426538f93083363b68721fbd17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602ae19a09fc688da052fdb003c20b9d",
+    "url": "/Avito-tech/index.html"
+  },
+  {
+    "revision": "6b53df7676c4716ba3c4",
+    "url": "/Avito-tech/static/css/main.1d8ed53c.chunk.css"
+  },
+  {
+    "revision": "62803952e3f2c4b4e41b",
+    "url": "/Avito-tech/static/js/2.ef06bda5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Avito-tech/static/js/2.ef06bda5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b53df7676c4716ba3c4",
+    "url": "/Avito-tech/static/js/main.f52c6f60.chunk.js"
+  },
+  {
+    "revision": "9fcdbb4e4b80713d00d9",
+    "url": "/Avito-tech/static/js/runtime-main.89f0821e.js"
+  },
+  {
+    "revision": "231985e925397b81f5f45579095bd9dd",
+    "url": "/Avito-tech/static/media/background.231985e9.jpg"
+  }
+]);
